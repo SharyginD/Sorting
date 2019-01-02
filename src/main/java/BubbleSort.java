1274b3fa477ad909d83dbb9ed7cb672e50cb5c83
SharyginD <dima.sharygin.1999@mail.ru>
2019-01-03 02:20:53 +0300

    add bubble sort

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,15 @@
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int x = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = x;
+                }
+            }
+        }
+    }
+}
